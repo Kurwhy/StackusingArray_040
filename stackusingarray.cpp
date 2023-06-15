1dd1f0634fcb66e6dfa714d042ccd487d584599b
Wahyu <firmansyahw503@gmail.com>
2023-06-15 11:10:23 +0700

    membuat metode pop untuk menghapus

--- stackusingarray.cpp
+++ stackusingarray.cpp
@@ -28,3 +28,12 @@ public:
 		return element;
 	}
 
+	void pop() {
+		if (empty()) {
+			cout << "\nStack is empty. Cannot pop." << endl;
+			return;
+		}
+
+		cout << "\nThe popped element is: " << stack_array[top] << endl;
+		top--;
+	}
