0e0a2ae463487b33021c2ae3c821511f4f5b675a
Wahyu <firmansyahw503@gmail.com>
2023-06-15 11:08:51 +0700

    membuat metode push

--- stackusingarray.cpp
+++ stackusingarray.cpp
@@ -8,4 +8,23 @@ private:
 	string stack_array[5];
 	int top;
 
-}
+public:
+	Program() {
+		top = -1;
+	}
+
+	string push(string element) {
+		if (top == 4) {
+			cout << "Stack Full" << endl;
+			return "";
+		}
+
+		top++;
+		stack_array[top] = element;
+
+		cout << endl;
+		cout << element << "ditambahkan (pushed)." << endl;
+
+		return element;
+	}
+
